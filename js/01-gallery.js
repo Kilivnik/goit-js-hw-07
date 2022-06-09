845a25404a90a444d712b6e9414d3a8429111bf6
Kilivnik <ekstasy1990@gmail.com>
2022-06-09 11:55:19 +0300

    add js, html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+galleryContainerEl.insertAdjacentHTML(
+  "beforeend",
+  galleryItemsEl(galleryItems)
+);
+function galleryItemsEl(list) {
+  return list
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+          </a>
+        </div>`
+    )
+    .join("");
+}
+galleryContainerEl.addEventListener("click", openModalGallery);
+
+function openModalGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="800" height="600" alt=${event.target.alt}>`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", galleryEscKey);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", galleryEscKey);
+      },
+    }
+  );
+  instance.show();
+  function galleryEscKey() {
+    if (event.key === "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+galleryContainerEl.insertAdjacentHTML(
+  "beforeend",
+  galleryItemsEl(galleryItems)
+);
+function galleryItemsEl(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`;
+    })
+    .join("");
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
